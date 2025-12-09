020b799c98d0b5b26402be648cbbbeb947dcd7c0
Sergey Chernyshev <schernyshev@cloudflare.com>
2025-12-09 15:03:30 -0500

    Added continue option

--- __tests__/delay.test.js
+++ __tests__/delay.test.js
@@ -3,23 +3,27 @@ import { BrowserConfig } from '../lib/browsers.js';
 
 const browsers = BrowserConfig.getBrowsers();
 
-describe.each(browsers)('Delaying first byte of the response - %s', browser => {
-  test('launchTest executes and delays .CSS responses by 2000ms', async () => {
-    const result = await launchTest({
-      url: 'https://www.speedpatterns.com/',
-      browser: browser,
-      delayFirstByte: { '.css$': 2000 },
-    });
+describe.each(['chrome'])(
+  'Delaying first byte of the response - %s',
+  browser => {
+    test('launchTest executes and delays .CSS responses by 2000ms', async () => {
+      const result = await launchTest({
+        url: 'https://www.speedpatterns.com/',
+        browser: browser,
+        debug: true,
+        delayFirstByte: { '**/*.css$': 2000 },
+      });
 
-    expect(result).toHaveProperty('success');
-    expect(result).toHaveProperty('testId');
-    expect(result).toHaveProperty('resultsPath');
-    expect(result.success).toBe(true);
+      expect(result).toHaveProperty('success');
+      expect(result.success).toBe(true);
+      expect(result).toHaveProperty('testId');
+      expect(result).toHaveProperty('resultsPath');
 
-    expect(
-      result.runner.resourceTimings
-        .filter(r => r.name.match('.css$'))
-        .every(r => r.duration >= 2000),
-    ).toBe(true);
-  }, 60000);
-});
+      expect(
+        result.runner.resourceTimings
+          .filter(r => r.name.match('.css$'))
+          .every(r => r.duration >= 2000),
+      ).toBe(true);
+    }, 60000);
+  },
+);
