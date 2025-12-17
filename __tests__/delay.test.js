aff3404ed60250b94770c6db341b1462b0a76f8f
Sergey Chernyshev <schernyshev@cloudflare.com>
2025-12-16 20:38:10 -0500

    Run delay test in all browsers

--- __tests__/delay.test.js
+++ __tests__/delay.test.js
@@ -4,14 +4,14 @@ import { DELAY_IMPLEMENTATIONS } from '../lib/delay.js';
 
 const browsers = BrowserConfig.getBrowsers();
 
-describe.each(['firefox'])('Delaying response - %s', browser => {
+describe.each(browsers)('Delaying response - %s', browser => {
   test.each(Object.keys(DELAY_IMPLEMENTATIONS))(
     'launchTest delays .CSS responses by 2000ms (using "%s" method)',
     async delayImplementationName => {
       const result = await launchTest({
         url: 'https://www.speedpatterns.com/',
         browser: browser,
-        debug: true,
+        // debug: true,
         delay: { '\.css$': 2000 },
         delayUsing: delayImplementationName,
       });
