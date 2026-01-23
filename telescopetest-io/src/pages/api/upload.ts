84696b96e70532d763079fe34cf95e71755d46dc
Judyzc <87judy@gmail.com>
2026-01-23 14:27:34 -0600

    working nav bar with empty pages

---
+++ telescopetest-io/src/pages/api/upload.ts
@@ -0,0 +1,6 @@
+import type { APIRoute } from 'astro';
+import { TestConfig, TestSource, TestStatus } from '../../types/testConfig';
+
+export const POST: APIRoute = async (context: any) => {
+    
+};
