dfdd3ee33efed28019dd4a842e60adf03af7a23e
Judyzc <87judy@gmail.com>
2026-01-29 16:05:21 -0600

    changed R2 store from zipKey/ to testId/, cleaned up some unnecessary D1 and R2 abstraction functions

--- telescopetest-io/src/lib/r2/r2-client.ts
+++ telescopetest-io/src/lib/r2/r2-client.ts
@@ -3,16 +3,8 @@ export class R2Client {
   constructor(bucketBinding: any) {
     this.bucket = bucketBinding;
   }
+  
   async put(key: string, value: any) {
     return await this.bucket.put(key, value);
-  }
-  async get(key: string) {
-    return await this.bucket.get(key);
-  }
-  async list(options?: { prefix?: string; limit?: number }) {
-    return await this.bucket.list(options);
-  }
-  async delete(key: string) {
-    return await this.bucket.delete(key);
   }
 }
