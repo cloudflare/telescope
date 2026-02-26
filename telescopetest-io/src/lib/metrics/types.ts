5052af6f78c0b20f393aa13e33f22b801a9f83ea
Judyzc <87judy@gmail.com>
2026-02-26 10:30:18 -0600

    condensed metrics, fixed name

--- telescopetest-io/src/lib/metrics/types.ts
+++ telescopetest-io/src/lib/metrics/types.ts
@@ -1,18 +1,95 @@
 /**
- * Structure of metrics.json from Telescope test results
+ * Structure of metrics.json from Telescope test results.
+ * Matches the actual output written by testRunner.ts collectMetrics().
  */
 export interface MetricsJson {
   navigationTiming?: {
-    navigationStart?: number;
+    name?: string;
+    entryType?: string;
+    startTime?: number;
+    duration?: number;
+    initiatorType?: string;
+    deliveryType?: string;
+    nextHopProtocol?: string;
+    renderBlockingStatus?: string;
+    contentEncoding?: string;
+    workerStart?: number;
+    redirectStart?: number;
+    redirectEnd?: number;
+    fetchStart?: number;
+    domainLookupStart?: number;
+    domainLookupEnd?: number;
+    connectStart?: number;
+    connectEnd?: number;
+    secureConnectionStart?: number;
+    requestStart?: number;
     responseStart?: number;
+    firstInterimResponseStart?: number;
+    finalResponseHeadersStart?: number;
     responseEnd?: number;
     transferSize?: number;
     encodedBodySize?: number;
-    duration?: number;
-    firstInterimResponseStart?: number;
-    finalResponseHeadersStart?: number;
+    decodedBodySize?: number;
+    responseStatus?: number;
+    serverTiming?: Array<{
+      name: string;
+      duration: number;
+      description?: string;
+    }>;
+    unloadEventStart?: number;
+    unloadEventEnd?: number;
+    domInteractive?: number;
+    domContentLoadedEventStart?: number;
+    domContentLoadedEventEnd?: number;
+    domComplete?: number;
+    loadEventStart?: number;
+    loadEventEnd?: number;
+    type?: string;
+    redirectCount?: number;
+    activationStart?: number;
   };
-  paintTiming?: Array<{ name: string; startTime: number }>;
-  largestContentfulPaint?: Array<{ startTime?: number }>;
-  layoutShifts?: Array<{ value?: number }>;
+  paintTiming?: Array<{
+    name: string;
+    entryType: string;
+    startTime: number;
+    duration: number;
+  }>;
+  userTiming?: Array<{
+    name: string;
+    entryType: 'mark' | 'measure';
+    startTime: number;
+    duration: number;
+  }>;
+  largestContentfulPaint?: Array<{
+    name?: string;
+    entryType?: string;
+    startTime?: number;
+    size?: number;
+    url?: string;
+    id?: string;
+    loadTime?: number;
+    renderTime?: number;
+    element?: {
+      nodeName?: string;
+      outerHTML?: string;
+      boundingRect?: {
+        x: number;
+        y: number;
+        width: number;
+        height: number;
+        top: number;
+        right: number;
+        bottom: number;
+        left: number;
+      };
+    };
+  }>;
+  layoutShifts?: Array<{
+    name?: string;
+    entryType?: string;
+    startTime?: number;
+    value?: number;
+    hadRecentInput?: boolean;
+    lastInputTime?: number;
+  }>;
 }
