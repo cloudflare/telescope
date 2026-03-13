bc05c751cd7f23658196aa0d55b67633ed814562
Judyzc <87judy@gmail.com>
2026-03-13 09:54:37 -0500

    fill in 'content_rating' column for existing rows all as 'safe' as part of 2nd migration

--- telescopetest-io/migrations/0002_add_content_rating.sql
+++ telescopetest-io/migrations/0002_add_content_rating.sql
@@ -14,7 +14,7 @@ CREATE TABLE "new_tests" (
     "created_at" INTEGER DEFAULT (unixepoch()),
     "updated_at" INTEGER DEFAULT (unixepoch())
 );
-INSERT INTO "new_tests" ("browser", "content_rating", "created_at", "description", "name", "source", "test_date", "test_id", "updated_at", "url", "zip_key") SELECT "browser", "content_rating", "created_at", "description", "name", "source", "test_date", "test_id", "updated_at", "url", "zip_key" FROM "tests";
+INSERT INTO "new_tests" ("browser", "created_at", "description", "name", "source", "test_date", "test_id", "updated_at", "url", "zip_key") SELECT "browser", "created_at", "description", "name", "source", "test_date", "test_id", "updated_at", "url", "zip_key" FROM "tests";
 DROP TABLE "tests";
 ALTER TABLE "new_tests" RENAME TO "tests";
 CREATE UNIQUE INDEX "tests_zip_key_key" ON "tests"("zip_key");
@@ -22,5 +22,6 @@ CREATE INDEX "idx_tests_file_key" ON "tests"("zip_key");
 CREATE INDEX "idx_tests_content_rating" ON "tests"("content_rating");
 CREATE INDEX "idx_tests_updated_at" ON "tests"("updated_at" DESC);
 CREATE INDEX "idx_tests_created_at" ON "tests"("created_at" DESC);
+UPDATE "tests" SET "content_rating" = 'safe' WHERE "content_rating" = 'unknown';
 PRAGMA foreign_keys=ON;
 PRAGMA defer_foreign_keys=OFF;
